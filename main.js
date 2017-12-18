72c06a3556c9bded76601a1c161776c06af73158
Khaleel Haboub <Khaleel@indielabs.sa>
2017-12-18 15:46:05 +0300

    Add a simple alidation layer

--- main.js
+++ main.js
@@ -34,6 +34,24 @@ class Blockchain{
 		newBlock.hash = newBlock.calculateHash();
 		this.chain.push(newBlock);
 	}
+
+	isBlockValid(){
+		for(let i = 1; i < this.chain.length; i++){
+			const currentBlock = this.chain[i];
+			const previousBlock = this.chain[i - 1];
+
+			if(currentBlock.hash !== currentBlock.calculateHash()){
+				return false;
+			}
+
+			if(currentBlock.previousHash !== previousBlock.hash){
+				return false;
+			}
+
+			return true;
+		}
+
+	}
 }
 
 let kalCoin = new Blockchain();
@@ -41,3 +59,4 @@ kalCoin.addBlock(new Block(1, "01/01/2018", {doc:"private data 1"}));
 kalCoin.addBlock(new Block(2, "10/01/2018", {doc:"private data 2"}));
 
 console.log(JSON.stringify(kalCoin, null, 4));
+console.log("is blockchain valid: " + JSON.stringify(kalCoin.isBlockValid(), null, 4));
